c62bb6ae7fa6abd4a1f8cdfbe1c39fe82535ee23
iimuli <noka567@hotmail.com>
2023-03-03 12:01:59 +0200

    Update PlayerControl.cs

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -1,12 +1,30 @@
-string lauri;
-string king;
 bool winGame = false;
+bool gameOver = false;
 
+int randomMinNumber;
+int randomMaxNumber;
+int randomNumber;
+
+
+void Update()
+{
+    if(Input.GetKey(KeyCode.Space))
+    {
+        King();
+    }
+}
 void King()
 {
-    if(lauri == king)
+    randomMaxNumber = 10;
+    randomMinNumber = 0;
+
+    randomNumber = RandomRange(randomMinNumber, randomMaxNumber);
+
+
+    if (randomNumber => 8)
     {
         Debug.Log("Lauri is a KING!");
         winGame = true;
     }
+    else gameOver = true;
 }
